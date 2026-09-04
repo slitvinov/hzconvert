a6e986135ec4c7aadea68d5ef1fe8f4e1bd0fb00
Sergey Litvinov <slitvinov@gmail.com>
2026-09-04 09:48:29 -0400

    check errors

--- summary.py
+++ summary.py
@@ -1,13 +1,21 @@
 import pandas, sys
-df = pandas.read_csv(sys.argv[1])
+try:
+    df = pandas.read_csv(sys.argv[1])
+except IndexError:
+    sys.stderr.write("summary.py: error: needs an argument\n")
+    exit(1)
+except FileNotFoundError as e:
+    sys.stderr.write("summary.py: error: fail to read '%s'\n" % e.filename)
+    exit(1)
 num = df.select_dtypes("number")
 summary = pandas.DataFrame({
-    "obs":    num.count(),
-    "mean":   num.mean(),
-    "std":   num.std(),
-    "min":    num.min(),
-    "max":    num.max(),
-    "nan":    num.isna().sum(),
+    "obs": num.count(),
+    "mean": num.mean(),
+    "std": num.std(),
+    "min": num.min(),
+    "max": num.max(),
+    "nan": num.isna().sum(),
     "unique": num.nunique(),
 })
 print(summary.to_string(float_format="{:.1f}".format))
+
